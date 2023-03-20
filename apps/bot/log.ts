4da8f1aecb630cce43717496d21e1bb3d931a1a2
Rafael Almeida <rafaelalmeidatk@gmail.com>
2023-03-20 00:08:02 -0300

    Add colors to debug when running with turborepo

--- apps/bot/log.ts
+++ apps/bot/log.ts
@@ -5,4 +5,7 @@ if (env.NODE_ENV === "development") {
   debug.enable("discord,discord:*");
 }
 
+// https://github.com/debug-js/debug/issues/922#issuecomment-1374524350
+(debug as any).useColors = () => true;
+
 export const baseLog = debug("discord");
