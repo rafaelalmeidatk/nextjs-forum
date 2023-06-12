a8fb068aba287273a593c9b757aae80228ed9a33
Brace Sproul <braceasproul@gmail.com>
2023-06-12 15:24:55 -0700

    fix: make entire post clickable

--- apps/web/components/post.tsx
+++ apps/web/components/post.tsx
@@ -26,34 +26,33 @@ export const Post = ({
   const borderColor = hasAnswer ? 'border-green-700' : 'border-neutral-700'
 
   return (
-    <div className={`px-4 py-3 border bg-neutral-800 ${borderColor} rounded`}>
-      <Link
-        className="text-white inline-block pr-2 text-lg font-semibold hover:opacity-75 hover:no-underline"
-        href={`/post/${id}`}
-      >
-        {title}
-      </Link>
+    <Link href={`/post/${id}`}>
+      <div className={`px-4 py-3 border bg-neutral-800 ${borderColor} rounded hover:opacity-75`}>
+        <p className="text-white inline-block pr-2 text-lg font-semibold hover:opacity-75">
+          {title}
+        </p>
 
-      <div className="mt-2 flex items-center space-x-2">
-        <img
-          src={author.avatar}
-          alt={`${author.username}'s avatar`}
-          className="rounded-full w-5 h-5"
-        />
-        <div className="text-sm opacity-90">
-          {author.username} asked on{' '}
-          <time dateTime={createdAtTimes.iso} title={createdAtTimes.tooltip}>
-            {createdAtTimes.text}
-          </time>{' '}
-          · {messagesCount} {plur('Message', messagesCount)}
-          {hasAnswer && (
-            <span>
-              {' '}
-              · <span className="font-semibold text-green-500">Answered</span>
-            </span>
-          )}
+        <div className="mt-2 flex items-center space-x-2">
+          <img
+            src={author.avatar}
+            alt={`${author.username}'s avatar`}
+            className="rounded-full w-5 h-5"
+          />
+          <div className="text-sm opacity-90">
+            {author.username} asked on{' '}
+            <time dateTime={createdAtTimes.iso} title={createdAtTimes.tooltip}>
+              {createdAtTimes.text}
+            </time>{' '}
+            · {messagesCount} {plur('Message', messagesCount)}
+            {hasAnswer && (
+              <span>
+                {' '}
+                · <span className="font-semibold text-green-500">Answered</span>
+              </span>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
