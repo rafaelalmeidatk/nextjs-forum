64476fc9632e8df8738fe8cd927011e1df0846ad
Brace Sproul <braceasproul@gmail.com>
2023-06-12 15:51:37 -0700

    fix: opacity-90 instead of -75

--- apps/web/components/post.tsx
+++ apps/web/components/post.tsx
@@ -27,7 +27,7 @@ export const Post = ({
 
   return (
     <Link href={`/post/${id}`} className='block text-white no-underline'>
-      <div className={`px-4 py-3 border bg-neutral-800 ${borderColor} rounded hover:opacity-75`}>
+      <div className={`px-4 py-3 border bg-neutral-800 ${borderColor} rounded hover:opacity-90`}>
         <p className="text-white inline-block pr-2 text-lg font-semibold">
           {title}
         </p>
