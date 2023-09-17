e9d843f34afa03b8b948dea742af8b1f0ac6801f
Vu Van Dung <me@joulev.dev>
2023-09-17 15:28:01 +0700

    improve local time display format
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/web/utils/datetime.ts
+++ apps/web/utils/datetime.ts
@@ -2,21 +2,27 @@ import { DateTime } from '@/utils/luxon'
 
 export const buildPostTimeValues = (createdAt: Date) => {
   const datetime = DateTime.fromJSDate(createdAt)
-  const text = datetime.toLocaleString(DateTime.DATETIME_SHORT)
-  const shortText = datetime.toLocaleString({
-    hour: 'numeric',
-    minute: 'numeric',
-  })
-  const tooltip = datetime.toLocaleString({
+  const text = datetime.setLocale('en-GB').toLocaleString({
     year: 'numeric',
     month: 'short',
     day: 'numeric',
     hour: 'numeric',
     minute: 'numeric',
+  }) // 17 Sep 2023 at 15:20
+  const shortText = datetime.toLocaleString({
+    hour: 'numeric',
+    minute: 'numeric',
+  }) // 15:20
+  const tooltip = datetime.setLocale('en-GB').toLocaleString({
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
     second: 'numeric',
     timeZoneName: 'short',
-  })
-  const iso = datetime.toISO()
+  }) // 17 September 2023 at 15:20:38 GMT+7
+  const iso = datetime.toISO() // 2023-09-17T15:20:38.000+07:00
 
   return { text, shortText, tooltip, iso }
 }
