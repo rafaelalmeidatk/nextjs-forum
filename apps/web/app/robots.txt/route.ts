4e6d6349a58180d330f0177fb8368feb61e93239
Michael H <56214343+RiskyMH@users.noreply.github.com>
2023-09-26 19:29:43 +1000

    use nextjs sitemap

--- apps/web/app/robots.txt/route.ts
+++
@@ -1,8 +0,0 @@
-import { getBaseUrl } from '@/utils/urls'
-
-export async function GET() {
-  const url = getBaseUrl()
-  return new Response(`User-agent: *
-Allow: /
-Sitemap: ${url}/sitemap.xml`)
-}
---
+++ apps/web/app/sitemap.ts
@@ -0,0 +1,37 @@
+import { MetadataRoute } from 'next'
+import { getBaseUrl } from '@/utils/urls'
+import { db } from '@nextjs-forum/db/node'
+
+// Update sitemap only once every 6 hours
+// export const revalidate = 21600
+
+export const revalidate = 0
+export const dynamic = 'force-dynamic'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await db
+    .selectFrom('posts')
+    .select(['posts.snowflakeId', 'posts.lastActiveAt'])
+    .limit(50_000) // we will probably need to chunk the sitemap in the future
+    .execute()
+
+  console.log(posts)
+
+  return [
+    {
+      url: getBaseUrl(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    ...posts.map((p) => {
+      return {
+        url: `${getBaseUrl()}/post/${p.snowflakeId}`,
+        changeFrequency: 'weekly',
+        priority: 0.9,
+        lastModified: p.lastActiveAt,
+      } satisfies MetadataRoute.Sitemap[0]
+    })
+
+  ]
+}
+
