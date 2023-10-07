04c0ec1047e1d39542d3fd240e204d0e49ce7f25
Michael H <git@riskymh.dev>
2023-10-08 06:21:18 +1100

    Unsolve command (#69)
    * Unsolve command
    
    * apply feedback
    
    * change command file
    
    * somehow didn't commit this

--- apps/bot/commands/slash/index.ts
+++ apps/bot/commands/slash/index.ts
@@ -1,9 +1,11 @@
 import * as refreshAnswerCount from './refresh-answer-count.js'
 import * as refreshLastActive from './refresh-last-active.js'
 import * as lockLowEffortPost from './lock-low-effort-post.js'
+import * as removePostAnswer from './remove-post-answer.js'
 
 export const slashCommands = [
   refreshAnswerCount.command,
   refreshLastActive.command,
   lockLowEffortPost.command,
+  removePostAnswer.command,
 ]
