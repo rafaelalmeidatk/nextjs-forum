a0f553cd7db729bc165e97e4d6980eccef9f710b
Alfonsus Ardani <alfonsusac@gmail.com>
2023-12-09 11:29:44 +0700

    add new icons

---
+++ apps/web/components/icons/message-count.tsx
@@ -0,0 +1,10 @@
+import { IconProps, IconSvg } from './base'
+
+export const MessageCountIcon = (props: IconProps) => (
+  <IconSvg viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M20 2H4a2 2 0 0 0-2 2v18l4-4h14a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2Z"
+    ></path>
+  </IconSvg>
+)
