c5a1e329ec80f8bf2c16cb9029137d0b507f86cf
Arinji2 <arinjaydhar205@gmail.com>
2024-12-31 00:02:51 +0530

    Use wrapper

--- apps/web/components/message.tsx
+++ apps/web/components/message.tsx
@@ -6,6 +6,7 @@ import { ShieldCheckIcon } from './icons/shield-check'
 import { DisplayLocalTime } from './local-time'
 import { Attachment, MessageContent } from './message-content'
 import { MessageReply, Reply } from '@/components/reply'
+import { MessageWrapper } from '@/components/message-wrapper'
 type MessageProps = {
   snowflakeId: string
   content: string
@@ -35,73 +36,75 @@ export const Message = ({
   const createdAtTimes = buildPostTimeValues(createdAt)
 
   return (
-    <div id={`message-${snowflakeId}`} className="group">
-      <div className="pointer-events-none flex w-full grow-0 flex-col items-stretch justify-start gap-2 p-2 [&>*]:pointer-events-auto">
-        {reply && <MessageReply reply={reply} />}
-        <div className="flex flex-row">
-          <div className="flex w-[50px] shrink-0 items-start justify-start sm:w-[60px]">
-            {isFirstRow ? (
-              <img
-                src={author.avatarUrl}
-                alt="Avatar"
-                className="h-10 w-10 rounded-full"
-              />
-            ) : (
-              <time
-                className="hidden w-full items-center self-center text-center text-xs opacity-70 group-hover:flex"
-                dateTime={createdAtTimes.iso}
-                title={createdAtTimes.tooltip}
-              >
-                <DisplayLocalTime short dateStr={createdAt.toISOString()} />
-              </time>
-            )}
-          </div>
-
-          <div className="w-0 flex-1">
-            {isFirstRow && (
-              <div className="flex items-center space-x-2">
-                <div className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
-                  {author.isPublic ? (
-                    <Link
-                      className="text-white opacity-90"
-                      href={`/user/${author.userID}`}
-                    >
-                      {author.username}
-                    </Link>
-                  ) : (
-                    <span className="opacity-50">{author.username}</span>
-                  )}
-
-                  {!author.isPublic && (
-                    <i title="User's profile isn't public">
-                      <IncognitoIcon className="pl-2" />
-                    </i>
-                  )}
-                  {author.isModerator && (
-                    <i title="User is a moderator">
-                      <ShieldCheckIcon className="pl-1" />
-                    </i>
-                  )}
-                  {author.isOP && (
-                    <span className="ml-2 select-none rounded-full bg-neutral-300 px-1 py-0.5 text-xs text-neutral-900">
-                      OP
-                    </span>
-                  )}
-                </div>
+    <MessageWrapper snowflakeId={snowflakeId}>
+      <div id={`message-${snowflakeId}`} className="group">
+        <div className="pointer-events-none flex w-full grow-0 flex-col items-stretch justify-start gap-2 p-2 [&>*]:pointer-events-auto">
+          {reply && <MessageReply reply={reply} />}
+          <div className="flex flex-row">
+            <div className="flex w-[50px] shrink-0 items-start justify-start sm:w-[60px]">
+              {isFirstRow ? (
+                <img
+                  src={author.avatarUrl}
+                  alt="Avatar"
+                  className="h-10 w-10 rounded-full"
+                />
+              ) : (
                 <time
-                  className="mt-[1px] text-xs opacity-70"
+                  className="hidden w-full items-center self-center text-center text-xs opacity-70 group-hover:flex"
                   dateTime={createdAtTimes.iso}
                   title={createdAtTimes.tooltip}
                 >
-                  <DisplayLocalTime dateStr={createdAt.toISOString()} />
+                  <DisplayLocalTime short dateStr={createdAt.toISOString()} />
                 </time>
-              </div>
-            )}
+              )}
+            </div>
 
-            <MessageContent content={content} attachments={attachments} />
+            <div className="w-0 flex-1">
+              {isFirstRow && (
+                <div className="flex items-center space-x-2">
+                  <div className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
+                    {author.isPublic ? (
+                      <Link
+                        className="text-white opacity-90"
+                        href={`/user/${author.userID}`}
+                      >
+                        {author.username}
+                      </Link>
+                    ) : (
+                      <span className="opacity-50">{author.username}</span>
+                    )}
+
+                    {!author.isPublic && (
+                      <i title="User's profile isn't public">
+                        <IncognitoIcon className="pl-2" />
+                      </i>
+                    )}
+                    {author.isModerator && (
+                      <i title="User is a moderator">
+                        <ShieldCheckIcon className="pl-1" />
+                      </i>
+                    )}
+                    {author.isOP && (
+                      <span className="ml-2 select-none rounded-full bg-neutral-300 px-1 py-0.5 text-xs text-neutral-900">
+                        OP
+                      </span>
+                    )}
+                  </div>
+                  <time
+                    className="mt-[1px] text-xs opacity-70"
+                    dateTime={createdAtTimes.iso}
+                    title={createdAtTimes.tooltip}
+                  >
+                    <DisplayLocalTime dateStr={createdAt.toISOString()} />
+                  </time>
+                </div>
+              )}
+
+              <MessageContent content={content} attachments={attachments} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </MessageWrapper>
   )
 }
