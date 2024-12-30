af604b95589e741342d958e8f9046313916f0cbc
Arinji2 <arinjaydhar205@gmail.com>
2024-12-30 19:34:26 +0530

    Add Reply Component

---
+++ apps/web/components/reply.tsx
@@ -0,0 +1,53 @@
+import { ImageIcon } from '@/components/icons/image'
+import { ReplySplineIcon } from '@/components/icons/reply-spline'
+import { Attachment } from '@/components/message-content'
+export type Reply = {
+  author: {
+    username: string
+    avatarUrl: string
+  }
+  messageID: string
+  content: string
+  attachments: Attachment[]
+}
+export const MessageReply = ({ reply }: { reply: Reply }) => {
+  return (
+    <a
+      href={`#message-${reply.messageID}`}
+      className="flex scroll-mt-[calc(50vh-50%)] flex-row scroll-smooth"
+    >
+      <div className="relative flex w-[50px] flex-col items-end justify-end sm:w-[60px]">
+        <ReplySplineIcon className="size-10 pt-2" />
+      </div>
+      <div className="flex w-0 flex-1 flex-row flex-nowrap items-center justify-start gap-2 text-xs md:text-base">
+        <img
+          src={reply.author.avatarUrl}
+          alt="Avatar"
+          className="size-5 shrink-0 rounded-full"
+        />
+
+        <p className="max-w-[100px] shrink-0 truncate opacity-50 md:max-w-full">
+          @{reply.author.username}
+        </p>
+        <p className="min-w-0 truncate text-sm opacity-70">
+          {reply.content.length == 0 && reply.attachments.length > 0 ? (
+            <span className="italic">Click to see attachment</span>
+          ) : reply.content.includes('```') ? (
+            <span className="d-code-inline !whitespace-nowrap">
+              {reply.content.replaceAll('```', '')}
+            </span>
+          ) : reply.content.includes('``') ? (
+            <span className="d-code-inline !whitespace-nowrap">
+              {reply.content.replaceAll('``', '')}
+            </span>
+          ) : (
+            reply.content
+          )}
+          {reply.attachments.length > 0 && (
+            <ImageIcon className="ml-2 size-5" />
+          )}
+        </p>
+      </div>
+    </a>
+  )
+}
