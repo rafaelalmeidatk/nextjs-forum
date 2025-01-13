61b654dba652cdd370825bc02fec060ed0890404
Arinji2 <arinjaydhar205@gmail.com>
2025-01-13 23:14:41 +0530

    Update function header + use state var

--- apps/web/components/user-avatar.client.tsx
+++ apps/web/components/user-avatar.client.tsx
@@ -1,14 +1,14 @@
 'use client'
-import { ImgHTMLAttributes } from 'react'
+import { ImgHTMLAttributes, useState } from 'react'
 import React from 'react'
-export function UserAvatar({
-  ...props
-}: {} & ImgHTMLAttributes<HTMLImageElement>) {
+const DEFAULT_AVATAR = 'https://cdn.discordapp.com/embed/avatars/1.png'
+export function UserAvatar(props: ImgHTMLAttributes<HTMLImageElement>) {
+  const [URL, setURL] = useState(props.src || DEFAULT_AVATAR)
   return (
     <img
-      onError={(e) => {
-        const url = 'https://cdn.discordapp.com/embed/avatars/1.png'
-        e.currentTarget.src = url
+      src={URL}
+      onError={() => {
+        setURL(DEFAULT_AVATAR)
       }}
       {...props}
     />
